51b09f13958cd436fca7e6fd4c451c871d1f10fc
maoweilin <92005340+maoweilin@users.noreply.github.com>
2025-08-13 21:27:37 +0800

    config.py

--- 毛炜林/week01/作业名/第13周作业/config.py
+++ 毛炜林/week01/作业名/第13周作业/config.py
@@ -5,22 +5,25 @@
 """
 
 Config = {
-    "model_path": "output",
-    "train_data_path": "data/train_tag_news.json",
-    "valid_data_path": "data/valid_tag_news.json",
+    "model_path": "model_output",
+    "schema_path": "ner_data/schema.json",
+    "train_data_path": "ner_data/train",
+    "valid_data_path": "ner_data/test",
     "vocab_path":"chars.txt",
     "model_type":"bert",
-    "max_length": 20,
-    "hidden_size": 128,
-    "kernel_size": 3,
+    "max_length": 100,
+    "hidden_size": 256,
     "num_layers": 2,
-    "epoch": 10,
-    "batch_size": 64,
-    "tuning_tactics":"lora_tuning",
-    # "tuning_tactics":"finetuing",
-    "pooling_style":"max",
+    "epoch": 20,
+    "batch_size": 16,
     "optimizer": "adam",
-    "learning_rate": 1e-3,
-    "pretrain_model_path":r"E:\八斗精品班\第六周 语言模型\bert-base-chinese\bert-base-chinese",
-    "seed": 987
+    "learning_rate": 5e-4,
+    "use_crf": True,
+    "num_labels": 9,
+    "use_bert":True,
+    "tuning_tactics":"lora_tuning",
+    "seed": 987,
+    "schema":9,
+    "pretrain_model_path": r"E:\八斗精品班\第六周 语言模型\bert-base-chinese\bert-base-chinese"
 }
+
