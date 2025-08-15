154d941aa5869b55164eb185b7ab7923e6bb7f42
syandupgzx <97205714+syandupgzx@users.noreply.github.com>
2025-08-15 21:15:42 +0800

    Create main.py

---
+++ 高子璇/week13/main.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import os
+import random
+import numpy as np
+import torch.nn as nn
+import logging
+from config import Config
+from model import TorchModel, choose_optimizer
+from evaluate import Evaluator
+from loader import load_data
+from peft import get_peft_model, LoraConfig
+
+#[DEBUG, INFO, WARNING, ERROR, CRITICAL]
+logging.basicConfig(level=logging.INFO, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+"""
+模型训练主程序
+"""
+
+seed = Config["seed"]
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+
+def main(config):
+    # 创建保存模型的目录
+    if not os.path.isdir(config["model_path"]):
+        os.mkdir(config["model_path"])
+    # 加载训练数据
+    train_data = load_data(config["train_data_path"], config)
+    # 加载模型
+    model = TorchModel
+
+    # 大模型微调策略
+    tuning_tactics = config["tuning_tactics"]
+    if tuning_tactics == "lora_tuning":
+        peft_config = LoraConfig(
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+            target_modules=["query", "key", "value"]
+        )
+    model = get_peft_model(model, peft_config)
+
+    # 标识是否使用gpu
+    cuda_flag = torch.cuda.is_available()
+    if cuda_flag:
+        logger.info("gpu可以使用，迁移模型至gpu")
